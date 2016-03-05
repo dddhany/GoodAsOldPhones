3df232935a429bcd558ab4f3ab28331b37f17c7f
Dhany Basuki <dddhany+github@gmail.com>
2016-03-05 08:14:38 +0800

    storyboard outlets

---
+++ GoodAsOldPhones/ProductViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProductViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Dhany Basuki on 5/3/16.
+//  Copyright © 2016 Dhany. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        productNameLabel.text = "1937 Desk Phone"
+        productImageView.image = UIImage(named: "phone-fullscreen3")
+    }
+}
